6c6fcd70c50110d8cc9d197a92217bf805a998ae
BUSSYJAY <omojoleoa@edutams.net>
2025-11-06 14:23:59 +0100

    ok123

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = 'https://effizy-fashion.giftedsociety.org/api/v1';
 
 // Generic API call function
 const apiCall = async (endpoint, options = {}) => {
